65fd12fc795f1d51a344d2d83b0cb213669b99e9
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-01 18:08:41 +0800

    overriding LineageOS properties
    Change-Id: I4627e2a876ece53faaa7402308255de88665be0c

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -248,7 +248,7 @@ def generate_build_info(args):
   # flavor (via a dedicated lunch config for example).
   print(f"ro.build.flavor={config['BuildFlavor']}")
 
-  print(f"ro.lineage.device={config['LineageDevice']}")
+  print(f"ro.axion.device={config['LineageDevice']}")
 
   # These values are deprecated, use "ro.product.cpu.abilist"
   # instead (see below).
